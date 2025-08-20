e8385a37817dd9072808f8345433beb124e08704
viniciusrc <vinaicius12@gmail.com>
2025-08-19 22:04:23 -0300

    modified:   MyFinance/app.js 	modified:   MyFinance/index.html 	new file:   MyFinance/login.html 	modified:   MyFinance/styles.css 	new file:   MyFinance/styles2.css

--- MyFinance/app.js
+++ MyFinance/app.js
@@ -1,15 +1,18 @@
 document.querySelectorAll('.dropdown-btn').forEach(btn => {
-        btn.addEventListener('click', function() {
-            alert(`Você clicou em: ${this.textContent}`);
-        });
+    btn.addEventListener('click', function () {
+        alert(`Você clicou em: ${this.textContent}`);
     });
+});
 
-    document.querySelector('.add-btn').addEventListener('click', function() {
-        const newBtn = document.createElement('button');
-        newBtn.className = 'dropdown-btn';
-        newBtn.textContent = 'Nova Conta';
-        newBtn.addEventListener('click', function() {
-            alert(`Você clicou em: ${this.textContent}`);
-        });
-        document.querySelector('.dropdown').appendChild(newBtn);
+document.querySelector('.add-btn').addEventListener('click', function () {
+    const newBtn = document.createElement('button');
+    newBtn.className = 'dropdown-btn';
+    const accountName = prompt("Digite o nome da nova conta:", "Nova Conta");
+    newBtn.textContent = accountName
+    newBtn.addEventListener('click', function () {
+        alert(`Você clicou em: ${this.textContent}`);
     });
+    document.querySelector('.dropdown').appendChild(newBtn);
+
+    
+});
