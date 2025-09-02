4ba72ead9057ac9aa30cf9528c6061d4aeaa2ffd
viniciusrc <vinaicius12@gmail.com>
2025-09-01 21:55:34 -0300

    modified:   app.js

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ document.querySelectorAll('.dropdown-btn').forEach(btn => {
 document.querySelector('.add-btn').addEventListener('click', function () {
     const newBtn = document.createElement('button');
     newBtn.className = 'dropdown-btn';
-    const accountName = prompt("Digite o nome da nova conta:", "Nova Conta");
-    newBtn.textContent = accountName
-    newBtn.addEventListener('click', function () {
+    const accountName = prompt("Digite o nome da nova conta:", "Nova Conta") 
+    const accountValue = parseFloat(prompt("Digite o valor da conta:", "100.00"));
+    const accountDate = prompt("Digite a data de vencimento da conta:", "DD/MM/AAAA");
+    newBtn.textContent = `${accountName} - R$ ${accountValue.toFixed(2)} - Vencimento: ${accountDate}`;
+    newBtn.addEventListener('click', function () {  
         alert(`Você clicou em: ${this.textContent}`);
     });
     document.querySelector('.dropdown').appendChild(newBtn);
 
-    
 });
